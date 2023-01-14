1bb445c6e83d383d48b36e5a316d29b8a7abcf6f
kaydee0922 <kentcabagua@gmail.com>
2023-01-14 14:17:33 +0800

    added register

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, TextInput, Pressable, Alert } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -20,7 +20,7 @@ function Login() {
                     height: 350,
                     width: 350,
                             }} 
-                source={require('../assets/main-logo.png')} 
+                source={require('../assets/main-icon/main-logo.png')} 
                 />
 
                 <Text style={styles.tagline}>Your Personalized Mechanic</Text>
@@ -32,7 +32,7 @@ function Login() {
                     <TextInput
                         style={{ 
                             padding: 5,
-                            paddingStart: 20,
+                            paddingStart: 40,
                             height: 50, 
                             width: "80%",
                             borderColor: 'red',
@@ -46,6 +46,15 @@ function Login() {
                         value={email}
                         placeholder="Email or Mobile Number"
                     />
+                     <View style={styles.userIcon}>
+                    <Image style={{
+                        resizeMode: "contain",
+                        height: 20,
+                        width: 20,
+                                }} 
+                        source={require('../assets/details-icon/user-icon.png')}
+                    />
+                    </View>
                 </View>
 
                 <View style={styles.passInput}>              
@@ -53,7 +62,7 @@ function Login() {
                         style={{ 
                             alignSelf: 'stretch',
                             padding: 5,
-                            paddingStart: 20,
+                            paddingStart: 40,
                             height: 50, 
                             width: "80%",
                             borderColor: 'red',
@@ -68,6 +77,17 @@ function Login() {
                         placeholder="Password"
                         secureTextEntry={passwordShown}
                     />
+
+                    <View style={styles.userPass}>
+                    <Image style={{
+                        resizeMode: "contain",
+                        height: 20,
+                        width: 20,
+                                }} 
+                        source={require('../assets/details-icon/pass-icon.png')}
+                    />
+                    </View>
+
                     <TouchableOpacity style={styles.eyecon} onPress={()=>{setPasswordShown((prev) => !prev)}}>
                         <Image style={{
                         resizeMode: "contain",
@@ -79,18 +99,32 @@ function Login() {
                     </TouchableOpacity>
                 </View> 
 
-                {/* <div className="login-password">
-                        <input type={passwordShown ? "text" : "password"} placeholder="Password" required />
-                        <button className="show-pass" onClick={togglePassword}><img src={eyecon} /></button>                         
-                    </div> */}
-
                 <View style={styles.forgotPass}>
                     <TouchableOpacity onPress={()=> navigation.navigate("")}>
                         <Text style={styles.forgotText}>Forgot Password</Text>
                     </TouchableOpacity>
                 </View>
-            
-            
+                
+                <View style={styles.logButton}>
+                    <Pressable
+                        style={styles.button}
+                        onPress={() =>Alert.alert('Welcome!')}
+                    >
+                    <Text style={styles.logIn}>
+                        LOG IN
+                    </Text>
+                    </Pressable>
+                    
+                </View>
+                
+                <View style={styles.register}>
+                    <Text style={styles.text}>Don't have an Account yet? Tap </Text>
+                    <TouchableOpacity onPress={()=> navigation.navigate("Register")}>
+                        <Text style={styles.regLink}>here </Text>
+                    </TouchableOpacity>
+                    <Text style={styles.text}>to Register.</Text>
+                </View>
+
             </View>
         </SafeAreaView>
     </ScrollView>
@@ -122,7 +156,7 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: '75%',
         color: 'white',
-        fontSize: 20,
+        fontSize: 18,
         fontWeight: '700',
         textShadowOffset: {width: 2, height: 2},
         textShadowColor: 'black',
@@ -138,8 +172,19 @@ const styles = StyleSheet.create({
     },
 
     userInput:{
-        width: "100%",
-        alignItems: 'center'
+        paddingTop: 8,
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: "100%"
+    },
+
+    userIcon:{
+        position: 'absolute',
+        paddingTop: 12,
+        left: 50
     },
 
     passInput:{
@@ -150,6 +195,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         width: "100%"
+    },
+
+    userPass:{
+        position: 'absolute',
+        paddingTop: 12,
+        left: 50
     },
 
     forgotPass:{
@@ -166,6 +217,47 @@ const styles = StyleSheet.create({
         position: 'absolute',
         paddingTop: 15,
         right: 50
+    },
+
+    logButton:{
+        paddingTop: 15,
+        width: 120
+    },
+
+    button:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 6,
+        paddingHorizontal: 35,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'red',
+    },
+
+    logIn:{
+        color:'white'
+    },
+
+    register:{
+        marginRight: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+        flexDirection: 'row',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        left: 12
+    },
+
+    text:{
+        fontSize: 12
+    },
+
+    regLink:{
+        fontSize: 14,
+        color: 'red'
     }
+
+
 
 })
